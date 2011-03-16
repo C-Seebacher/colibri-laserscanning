0a2268e67c0d272fe4b68c8cee52ecffbbf4da40
seebacher@biz.uni-muenchen.de <seebacher@biz.uni-muenchen.de@9c2808f5-1018-8e39-3883-5805d4c27408>
2011-03-16 19:23:03 +0000

    Tag for the actual stable version as there is new folder structure comming.

---
+++ tags/Version2010/2Photon/Binning DLL/code.c
@@ -0,0 +1,16 @@
+/* Call Library source file */
+
+#include "extcode.h"
+
+long BinningImage(unsigned char NChannels, unsigned long IMAQPointerList[], 
+	short int In[], unsigned long NResult, unsigned short int Binning, 
+	unsigned long *Position, unsigned long LinePixel[]);
+
+long BinningImage(unsigned char NChannels, unsigned long IMAQPointerList[], 
+	short int In[], unsigned long NResult, unsigned short int Binning, 
+	unsigned long *Position, unsigned long LinePixel[])
+	{
+
+	/* Insert code here */
+
+	}
